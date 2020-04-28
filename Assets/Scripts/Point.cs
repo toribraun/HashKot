58ee578492280494b79a4feef58edc4a0c9daace
victoriabraun <toribraun>
2020-04-28 23:01:04 +0500

    Added Points

---
+++ Assets/Scripts/Point.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point : MonoBehaviour
+{
+    [SerializeField]
+    private int cost = 1;
+    private SpriteRenderer sprite;
+    
+    public void Awake()
+    {
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        var unit = collider.GetComponent<HashKot>();
+        if (unit)
+        {
+            unit.GetPoints(cost);
+            Destroy(gameObject);
+        }
+    }
+}
